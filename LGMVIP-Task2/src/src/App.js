101f32100480e13788c1e7a6b99f4c9a17ff4ad1
Rahul Mulge <83878351+rahulmulge@users.noreply.github.com>
2022-09-09 14:40:12 +0530

    Add files via upload

---
+++ LGMVIP-Task2/src/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Users from './Users';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+         <h1> LGMVIP-Web(Task2) </h1>
+      </header>
+      <Users/>
+    </div>
+  );
+}
+
+export default App;
---
+++ LGMVIP-Task2/src/src/Users.js
@@ -0,0 +1,49 @@
+import React , {useState} from 'react'
+const Users = () => {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
+    
+  const load = async() => {
+    setLoading(true);
+    let response = await fetch('https://reqres.in/api/users');
+    let jsonRes = await response.json();
+    const data = jsonRes.data;
+    setUsers(data);
+    if(data)
+      setLoading(false);
+    console.log(users)
+  }
+  
+  const LoadingSpinner = () => {
+    <div className="spinner-container">
+      <div className="loading-spinner">
+      </div>
+    </div>
+  }
+
+  return (
+    <>
+    <button className='getData' onClick={load}>
+      Get Users
+    </button>
+    <div className='big'>
+      {loading? <LoadingSpinner/> : ""}
+      {
+        users?.map(  ({id , first_name, last_name ,email , avatar}) => {
+          return(
+          <ul className='card' key={id}> 
+          <img src={avatar} alt="Avatar"  />
+          <div class="container">
+            <h4><b> {first_name } {last_name} </b></h4> 
+            <p> 📧 {email}</p> 
+           </div>
+          </ul>
+  
+        )} )
+      }
+    </div>
+    </>
+  )
+}
+
+export default Users
