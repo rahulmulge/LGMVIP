b6b448f0d7644557d8a5e2675a9a1a46ce711a23
Rahul Mulge <83878351+rahulmulge@users.noreply.github.com>
2022-09-09 15:27:12 +0530

    Add files via upload

---
+++ LGMVIP-Task3/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Input from './components/Input';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>
+          Student Registeration
+        </h2>
+        <hr />
+      <h4>Personal Information</h4>
+      </header>
+      <Input />
+
+    </div>
+  );
+}
+
+export default App;
